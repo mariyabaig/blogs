92f87f40c53aa520dabd59808325b4da039363da
mariyabaig <mariyabaig0106@gmail.com>
2023-06-21 16:24:57 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <div className='bg-red-300'>Initial set up</div>
+      <Navbar/>
     </>
   );
 }
