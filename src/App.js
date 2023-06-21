599e3bdbe7c12c1c6c6937d69ffc3960307fba94
mariyabaig <mariyabaig0106@gmail.com>
2023-06-21 16:40:16 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from "react";
+import { Link, Routes, BrowserRouter, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import About from './pages/About';
+import List from './pages/List';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/addblogs" element={<Blogs />} />
+          <Route exact path="/about" element={<About />} />
+          <Route eaxct path="/list" element={<List />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
