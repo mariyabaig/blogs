f2b63a75e2e17919efa6893e40b4e34b09fab481
mariyabaig <mariyabaig0106@gmail.com>
2023-06-22 17:11:05 +0530

    Form data submitted and set in local storage using hook

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState({ title: "", category: "", context: "" });
+
+  const handleChange = (event)=>{
+   setBlogs({
+    ...blogs,
+    [event.target.name]:event.target.value
+    
+   })
+   localStorage.setItem('formValues', JSON.stringify({ ...blogs, [event.target.name]: event.target.value }));
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   console.log(blogs)
+   console.log("local storage", localStorage)
+   
+  };
+
   return (
     <>
       <div className="bg-blue-200 min-h-screen py-32 px-10">
         <div className="bg-white p-10 rounded-lg shadow m:w-3/4 lg:w-1/2 mx-auto">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="mb-5">
               <lable for="title" className="block mb-2 text-gray-500 font-bold">
                 Title
@@ -13,6 +30,9 @@ const Blogs = () => {
               <input
                 type="text"
                 id="title"
+                value={blogs.title}
+                onChange={handleChange}
+                name="title"
                 placeholder="what did i do today?"
                 className="border border-gray-300 p-3 w-full rounded shadow focus:outline-none focus:ring-2 focus:border-blue-300"
               />
@@ -27,6 +47,8 @@ const Blogs = () => {
               </lable>
               <input
                 type="text"
+                name="category"
+                value={blogs.category} onChange={handleChange}
                 id="category"
                 placeholder="Journal"
                 className="border border-gray-300 p-3 w-full rounded shadow focus:outline-none focus:ring-2 focus:border-blue-300"
@@ -46,7 +68,9 @@ const Blogs = () => {
               <input
                 type="text"
                 id="content"
-
+                name="context"
+                value={blogs.context}
+                onChange={handleChange}
                 className="border border-gray-300 p-3 w-full rounded shadow focus:outline-none focus:ring-2 focus:border-blue-300"
               />
 
@@ -54,7 +78,12 @@ const Blogs = () => {
                 Twitter username is required
               </p> */}
             </div>
-            <button className="block w-full bg-blue-400 p-4 text-white font-bold rounded-lg shadow hover:shadow-lg">
+            <button
+              onClick={() => {
+                console.log("Submitted");
+              }}
+              className="block w-full bg-blue-400 p-4 text-white font-bold rounded-lg shadow hover:shadow-lg"
+            >
               Submit
             </button>
           </form>
@@ -62,8 +91,6 @@ const Blogs = () => {
       </div>
 
       <hr />
-
-      
     </>
   );
 };
