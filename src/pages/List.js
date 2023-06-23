59b66774fb9d51e8f499a4aaf453a44d8f22114a
mariyabaig <mariyabaig0106@gmail.com>
2023-06-23 17:16:54 +0530

    List items styled

--- src/pages/List.js
+++ src/pages/List.js
@@ -8,14 +8,14 @@ const List = () => {
   return (
     <>
 {posts && posts.length > 0 && (
-        <div className="mt-4">
+        <div className="mt-4 grid grid-cols-3 gap-2">
           <h2 className="text-xl font-bold mb-2">Submitted Posts:</h2>
           <ul>
             {posts.map((post, index) => (
-              <li key={index} className="mb-4">
-                <h3 className="text-lg font-bold">{post.title}</h3>
+              <li key={index} className="mb-4 bg-gray-200 p-16 rounded-md ">
+                <h3 className="text-lg font-bold">Title: {post.title}</h3>
                 <p className="text-gray-500">Category: {post.category}</p>
-                <p>{post.context}</p>
+                <p>Context: {post.context}</p>
               </li>
             ))}
           </ul>
