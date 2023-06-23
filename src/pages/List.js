9d996bd30504e5af8cf9abdc5e2950c5f631abce
mariyabaig <mariyabaig0106@gmail.com>
2023-06-23 17:28:05 +0530

    Shadoes and buttons added

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addPost } from '../state/action-creators/index';
 
 const List = () => {
+  const posts = useSelector((state) => state.blogReducer.posts);
 
-  const posts = useSelector((state)=>state.blogReducer.posts)
   return (
     <>
-{posts && posts.length > 0 && (
-        <div className="mt-4 grid grid-cols-3 gap-2">
-          <h2 className="text-xl font-bold mb-2">Submitted Posts:</h2>
+    <div className='h-screen  bg-gray-100'>
+      <h2 className="text-xl font-bold mb-2 ">Submitted Posts:</h2>
+      {posts && posts.length > 0 && (
+        <div className="mt-4 grid grid-cols-2 gap-2">
           <ul>
             {posts.map((post, index) => (
-              <li key={index} className="mb-4 bg-gray-200 p-16 rounded-md ">
+              <li
+                key={index}
+                className="m-4 p-8 rounded-md shadow-md bg-white"
+              >
                 <h3 className="text-lg font-bold">Title: {post.title}</h3>
-                <p className="text-gray-500">Category: {post.category}</p>
-                <p>Context: {post.context}</p>
+                <p className="text-lg">Category: {post.category}</p>
+                <p className="text-lg">Context: {post.context}</p>
+                <button className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm">
+                  Edit
+                </button>
+                <button className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm">
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
         </div>
       )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default List;
