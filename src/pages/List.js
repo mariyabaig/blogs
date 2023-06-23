26188d61dde2d247b4b468017127b5250fed2db8
mariyabaig <mariyabaig0106@gmail.com>
2023-06-23 17:09:46 +0530

    Displaying submitted blogs successfully

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { addPost } from '../state/action-creators/index';
 
 const List = () => {
+
+  const posts = useSelector((state)=>state.blogReducer.posts)
   return (
-    <div>List</div>
+    <>
+{posts && posts.length > 0 && (
+        <div className="mt-4">
+          <h2 className="text-xl font-bold mb-2">Submitted Posts:</h2>
+          <ul>
+            {posts.map((post, index) => (
+              <li key={index} className="mb-4">
+                <h3 className="text-lg font-bold">{post.title}</h3>
+                <p className="text-gray-500">Category: {post.category}</p>
+                <p>{post.context}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
 
