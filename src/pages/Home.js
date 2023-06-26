ddefc4c53734cd428e026605b22f9ce784827278
mariyabaig <mariyabaig0106@gmail.com>
2023-06-26 11:36:10 +0530

    Home page modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold mb-4">Blog Away! Share Your Thoughts.</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => navigate('/addblogs')}
+      >
+        Add Blogs
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
