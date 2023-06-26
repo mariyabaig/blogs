b9830553a0dfeef3cabc6980a5732c3e8afa8988
mariyabaig <mariyabaig0106@gmail.com>
2023-06-26 12:34:20 +0530

    UI modified

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,22 +1,168 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deletePost, editPost } from '../state/action-creators/index';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp as solidThumbsUp } from '@fortawesome/free-solid-svg-icons';
-import { faThumbsUp as regularThumbsUp } from '@fortawesome/free-regular-svg-icons';
-import { useNavigate } from 'react-router-dom';
+// import React, { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { deletePost, editPost } from "../state/action-creators/index";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faThumbsUp as solidThumbsUp } from "@fortawesome/free-solid-svg-icons";
+// import { faThumbsUp as regularThumbsUp } from "@fortawesome/free-regular-svg-icons";
+// import { useNavigate } from "react-router-dom";
+
+// const List = () => {
+//   const navigate = useNavigate();
+//   const posts = useSelector((state) => state.blogReducer.posts);
+//   const dispatch = useDispatch();
+
+//   const [editMode, setEditMode] = useState(false);
+//   const [editedIndex, setEditedIndex] = useState(null);
+//   const [editedTitle, setEditedTitle] = useState("");
+//   const [editedCategory, setEditedCategory] = useState("");
+//   const [editedContext, setEditedContext] = useState("");
+
+//   const handleDeletePost = (index) => {
+//     dispatch(deletePost(index));
+//   };
+
+//   const handleEditPost = (index) => {
+//     setEditMode(true);
+//     setEditedIndex(index);
+//     const post = posts[index];
+//     setEditedTitle(post.title);
+//     setEditedCategory(post.category);
+//     setEditedContext(post.context);
+//   };
+
+//   const handleSaveEdit = () => {
+//     console.log("Edited Index:", editedIndex);
+//     console.log("Edited Title:", editedTitle);
+//     console.log("Edited Category:", editedCategory);
+//     console.log("Edited Context:", editedContext);
+//     dispatch(editPost(editedIndex, editedTitle, editedCategory, editedContext));
+//     setEditMode(false);
+//     setEditedIndex(null);
+//     setEditedTitle("");
+//     setEditedCategory("");
+//     setEditedContext("");
+//   };
+
+//   const handleCancelEdit = () => {
+//     setEditMode(false);
+//     setEditedIndex(null);
+//     setEditedTitle("");
+//     setEditedCategory("");
+//     setEditedContext("");
+//   };
+//   const [likedPosts, setLikedPosts] = useState([]);
+
+//   const handleToggleLike = (index) => {
+//     if (likedPosts.includes(index)) {
+//       setLikedPosts(likedPosts.filter((likedIndex) => likedIndex !== index));
+//     } else {
+//       setLikedPosts([...likedPosts, index]);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="h-screen bg-gray-100">
+//         <h2 className="text-xl font-bold mb-2">Submitted Posts:</h2>
+//         {posts && posts.length > 0 && (
+//           <div className="mt-4 grid grid-cols-2 gap-2">
+//             <ul>
+//               {posts.map((post, index) => (
+//                 <li
+//                   key={index}
+//                   className="m-4 p-8 rounded-md shadow-md bg-white"
+//                 >
+//                   {editMode && index === editedIndex ? (
+//                     <>
+//                       <input
+//                         type="text"
+//                         value={editedTitle}
+//                         onChange={(e) => setEditedTitle(e.target.value)}
+//                         className="mb-2 px-2 py-1 rounded border"
+//                       />
+//                       <input
+//                         type="text"
+//                         value={editedCategory}
+//                         onChange={(e) => setEditedCategory(e.target.value)}
+//                         className="mb-2 px-2 py-1 rounded border"
+//                       />
+//                       <input
+//                         type="text"
+//                         value={editedContext}
+//                         onChange={(e) => setEditedContext(e.target.value)}
+//                         className="mb-2 px-2 py-1 rounded border"
+//                       />
+//                       <button
+//                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+//                         onClick={handleSaveEdit}
+//                       >
+//                         Save
+//                       </button>
+
+//                       <button
+//                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+//                         onClick={handleCancelEdit}
+//                       >
+//                         Cancel
+//                       </button>
+//                     </>
+//                   ) : (
+//                     <>
+//                       <h3 className="text-lg font-bold">Title: {post.title}</h3>
+//                       <p className="text-lg">Category: {post.category}</p>
+//                       <p className="text-lg">Context: {post.context}</p>
+//                       <button
+//                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+//                         onClick={() => handleEditPost(index)}
+//                       >
+//                         Edit
+//                       </button>
+//                       <button
+//                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+//                         onClick={() => handleDeletePost(index)}
+//                       >
+//                         Delete
+//                       </button>
+//                       <button
+//                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+//                         onClick={() => handleToggleLike(index)}
+//                       >
+//                         {likedPosts.includes(index) ? (
+//                           <FontAwesomeIcon icon={solidThumbsUp} />
+//                         ) : (
+//                           <FontAwesomeIcon icon={regularThumbsUp} />
+//                         )}
+//                         Like
+//                       </button>
+//                     </>
+//                   )}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default List;
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deletePost, editPost } from "../state/action-creators/index";
+import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const List = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const posts = useSelector((state) => state.blogReducer.posts);
   const dispatch = useDispatch();
 
   const [editMode, setEditMode] = useState(false);
   const [editedIndex, setEditedIndex] = useState(null);
-  const [editedTitle, setEditedTitle] = useState('');
-  const [editedCategory, setEditedCategory] = useState('');
-  const [editedContext, setEditedContext] = useState('');
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedCategory, setEditedCategory] = useState("");
+  const [editedContext, setEditedContext] = useState("");
 
   const handleDeletePost = (index) => {
     dispatch(deletePost(index));
@@ -32,30 +178,26 @@ const List = () => {
   };
 
   const handleSaveEdit = () => {
-    console.log('Edited Index:', editedIndex);
-    console.log('Edited Title:', editedTitle);
-    console.log('Edited Category:', editedCategory);
-    console.log('Edited Context:', editedContext);
+    console.log("Edited Index:", editedIndex);
+    console.log("Edited Title:", editedTitle);
+    console.log("Edited Category:", editedCategory);
+    console.log("Edited Context:", editedContext);
     dispatch(editPost(editedIndex, editedTitle, editedCategory, editedContext));
     setEditMode(false);
     setEditedIndex(null);
-    setEditedTitle('');
-    setEditedCategory('');
-    setEditedContext('');
+    setEditedTitle("");
+    setEditedCategory("");
+    setEditedContext("");
   };
-  
-  
-  
-  
 
   const handleCancelEdit = () => {
     setEditMode(false);
     setEditedIndex(null);
-    setEditedTitle('');
-    setEditedCategory('');
-    setEditedContext('');
-
+    setEditedTitle("");
+    setEditedCategory("");
+    setEditedContext("");
   };
+
   const [likedPosts, setLikedPosts] = useState([]);
 
   const handleToggleLike = (index) => {
@@ -65,16 +207,19 @@ const List = () => {
       setLikedPosts([...likedPosts, index]);
     }
   };
-  
+
   return (
     <>
-      <div className='h-screen bg-gray-100'>
-        <h2 className="text-xl font-bold mb-2">Submitted Posts:</h2>
+      <div className="h-screen bg-gray-100">
+        <h2 className="text-3xl font-bold m-2 flex justify-center font-karla">Submitted Posts:</h2>
         {posts && posts.length > 0 && (
           <div className="mt-4 grid grid-cols-2 gap-2">
             <ul>
               {posts.map((post, index) => (
-                <li key={index} className="m-4 p-8 rounded-md shadow-md bg-white">
+                <li
+                  key={index}
+                  className="m-4 p-8 rounded-md shadow-md bg-white"
+                >
                   {editMode && index === editedIndex ? (
                     <>
                       <input
@@ -96,11 +241,11 @@ const List = () => {
                         className="mb-2 px-2 py-1 rounded border"
                       />
                       <button
-  className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-  onClick={handleSaveEdit}
->
-  Save
-</button>
+                        className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+                        onClick={handleSaveEdit}
+                      >
+                        Save
+                      </button>
 
                       <button
                         className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
@@ -127,17 +272,16 @@ const List = () => {
                         Delete
                       </button>
                       <button
-          className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-          onClick={() => handleToggleLike(index)}
-        >
-          {likedPosts.includes(index) ? (
-            <FontAwesomeIcon icon={solidThumbsUp} />
-          ) : (
-            <FontAwesomeIcon icon={regularThumbsUp} />
-          )}
-          Like
-        </button>
-     
+                        className="m-2 text-purple-500 shadow-sm"
+                        onClick={() => handleToggleLike(index)}
+                      >
+                        {likedPosts.includes(index) ? (
+                          <BsSuitHeartFill size={20} />
+                        ) : (
+                          <BsSuitHeart size={20} />
+                        )}
+                        
+                      </button>
                     </>
                   )}
                 </li>
