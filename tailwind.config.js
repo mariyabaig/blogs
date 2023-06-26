8c9ceb78f9505537aa2b3bc7ce8fff7f4617190b
mariyabaig <mariyabaig0106@gmail.com>
2023-06-26 11:47:09 +0530

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cursive': ['Cedarville Cursive', 'cursive'],
+        karla: ['Karla', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
