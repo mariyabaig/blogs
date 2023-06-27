c38d5498e546e24c1f57dfd03aa0dc4b6805d07b
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 01:13:00 +0530

    Layout fixed for edit and grid

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deletePost, editPost } from "../state/action-creators/index";
@@ -62,84 +61,81 @@ const List = () => {
 
   return (
     <>
-      <div className=" bg-gray-100">
+      <div className="bg-gray-100">
         <h2 className="text-3xl font-bold m-2 flex justify-center font-karla">Submitted Posts:</h2>
         <button className="bg-purple-100 p-4 text-purple-800 font-bold rounded-lg block m-3" onClick={()=>navigate("/addblogs")}>Add new blog</button>
         {posts && posts.length > 0 && (
-          <div className="mt-4 grid sm:grid-cols-2">
-            <ul>
-              {posts.map((post, index) => (
-                <li
-                  key={index}
-                  className="m-4 p-8 rounded-md shadow-md bg-white"
-                >
-                  {editMode && index === editedIndex ? (
-                    <>
-                      <input
-                        type="text"
-                        value={editedTitle}
-                        onChange={(e) => setEditedTitle(e.target.value)}
-                        className="mb-2 px-2 py-1 rounded border"
-                      />
-                      <input
-                        type="text"
-                        value={editedCategory}
-                        onChange={(e) => setEditedCategory(e.target.value)}
-                        className="mb-2 px-2 py-1 rounded border"
-                      />
-                      <input
-                        type="text"
-                        value={editedContext}
-                        onChange={(e) => setEditedContext(e.target.value)}
-                        className="mb-2 px-2 py-1 rounded border"
-                      />
-                      <button
-                        className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-                        onClick={handleSaveEdit}
-                      >
-                        Save
-                      </button>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 m-4">
+            {posts.map((post, index) => (
+              <div
+                key={index}
+                className="m-4 p-8 rounded-md shadow-md bg-white"
+              >
+                {editMode && index === editedIndex ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editedTitle}
+                      onChange={(e) => setEditedTitle(e.target.value)}
+                      className="mb-2 px-2 py-1 rounded border"
+                    />
+                    <input
+                      type="text"
+                      value={editedCategory}
+                      onChange={(e) => setEditedCategory(e.target.value)}
+                      className="mb-2 px-2 py-1 rounded border"
+                    />
+                    <input
+                      type="text"
+                      value={editedContext}
+                      onChange={(e) => setEditedContext(e.target.value)}
+                      className="mb-2 px-2 py-1 rounded border"
+                    />
+                    <button
+                      className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+                      onClick={handleSaveEdit}
+                    >
+                      Save
+                    </button>
 
-                      <button
-                        className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-                        onClick={handleCancelEdit}
-                      >
-                        Cancel
-                      </button>
-                    </>
-                  ) : (
-                    <>
-                      <h3 className="text-lg font-bold">Title: {post.title}</h3>
-                      <p className="text-lg">Category: {post.category}</p>
-                      <p className="text-lg">Context: {post.context}</p>
-                      <button
-                        className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-                        onClick={() => handleEditPost(index)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
-                        onClick={() => handleDeletePost(index)}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="m-2 text-purple-500 shadow-sm"
-                        onClick={() => handleToggleLike(index)}
-                      >
-                        {likedPosts.includes(index) ? (
-                          <BsSuitHeartFill size={20} />
-                        ) : (
-                          <BsSuitHeart size={20} />
-                        )}
-                        
-                      </button>
-                    </>
-                  )}
-                </li>
-              ))}
-            </ul>
+                    <button
+                      className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+                      onClick={handleCancelEdit}
+                    >
+                      Cancel
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <h3 className="text-lg font-bold">Title: {post.title}</h3>
+                    <p className="text-lg">Category: {post.category}</p>
+                    <p className="text-lg">Context: {post.context}</p>
+                    <button
+                      className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+                      onClick={() => handleEditPost(index)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="m-2 bg-blue-500 p-3 rounded text-white shadow-sm"
+                      onClick={() => handleDeletePost(index)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="m-2 text-purple-500 shadow-sm"
+                      onClick={() => handleToggleLike(index)}
+                    >
+                      {likedPosts.includes(index) ? (
+                        <BsSuitHeartFill size={20} />
+                      ) : (
+                        <BsSuitHeart size={20} />
+                      )}
+                    </button>
+                  </>
+                )}
+              </div>
+            ))}
           </div>
         )}
       </div>
