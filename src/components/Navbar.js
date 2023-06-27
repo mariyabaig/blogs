3c30357e3f0ec1668e739dc8bab401959bfa2259
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 01:46:20 +0530

    Current time added for better UX on nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const timeString = now.toLocaleTimeString();
+      setCurrentTime(timeString);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
-      <nav className=" shadow-lg bg-purple-200 flex justify-center font-karla">
+      <nav className="shadow-lg bg-purple-200 flex justify-between font-karla">
         <div className="max-w-3xl mx-auto">
           <div className="grid sm:grid-cols-4 text-lg gap-4 text-center py-4 px-8">
             <div className="">
-              <Link to="/" className="hover:font-bold p-2 font-cursive text-4xl font-extrabold text-purple-950 ">
+              <Link to="/" className="hover:font-bold p-2 font-cursive text-4xl font-extrabold text-purple-950">
                 BlogAway
               </Link>
             </div>
-          
             <div>
               <Link to="/addblogs" className="hover:font-bold p-2">
                 add blogs
@@ -30,8 +43,9 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-
-        
+        <div className="mr-8">
+          <span className="text-sm text-gray-500">{currentTime}</span>
+        </div>
       </nav>
       <div className="h-3 bg-purple-300"></div> {/* Colored div as navbar border */}
       <div className="h-3 bg-blue-200"></div> {/* Colored div as navbar border */}
