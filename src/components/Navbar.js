bab6202ebd85189f73b13540459e0bd2b26049f3
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 13:55:16 +0530

    Turned responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,42 +24,53 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="shadow-lg bg-purple-200 flex justify-between font-karla">
-        <div className="max-w-3xl mx-auto">
-          <div className="flex justify-between items-center px-4 py-2">
-            <Link to="/" className="hover:font-bold p-2 font-cursive text-4xl font-extrabold text-purple-950">
-              BlogAway
-            </Link>
-            <button
-              onClick={toggleMenu}
-              className="text-purple-950 hover:text-purple-900 focus:outline-none focus:text-purple-900"
-              aria-label="Toggle menu"
-            >
-              {menuOpen ? <FaTimes /> : <FaBars />}
-            </button>
-          </div>
-          <div
-            className={`${
-              menuOpen ? "flex text-sm " : "hidden"
-            } sm:flex sm:items-center sm:justify-between text-lg gap-4 text-center py-4 px-8`}
+      <nav className="bg-purple-200">
+        <div className="max-w-3xl mx-auto px-4 py-2 flex justify-between items-center">
+          <Link
+            to="/"
+            className="text-4xl font-bold text-purple-950 hover:font-bold font-cursive"
           >
-            <Link to="/addblogs" className="hover:font-bold p-2">
+            BlogAway
+          </Link>
+          <button
+            onClick={toggleMenu}
+            className="text-purple-950 hover:text-purple-900 focus:outline-none focus:text-purple-900 lg:hidden"
+            aria-label="Toggle menu"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+          <div className="hidden lg:flex lg:items-center lg:justify-between text-lg gap-4 text-center py-4 px-8 md:flex">
+          
+            <Link to="/addblogs" className="hover:font-bold" >
               Add Blogs
             </Link>
-            <Link to="/list" className="hover:font-bold p-2">
+            <Link to="/list" className="hover:font-bold">
               View Blogs
             </Link>
-            <Link to="/about" className="hover:font-bold p-2">
+            <Link to="/about" className="hover:font-bold">
               About
             </Link>
           </div>
         </div>
-        <div className="mr-8">
-          <span className="text-sm text-gray-500">{currentTime}</span>
-        </div>
+        {menuOpen && (
+          <div className="lg:hidden bg-gray-100 py-4 px-8">
+            <Link to="/addblogs" className="block py-2">
+              Add Blogs
+            </Link>
+            <Link to="/list" className="block py-2">
+              View Blogs
+            </Link>
+            <Link to="/about" className="block py-2">
+              About
+            </Link>
+          </div>
+        )}
       </nav>
-      <div className="h-3 bg-purple-300"></div> {/* Colored div as navbar border */}
-      <div className="h-3 bg-blue-200"></div> {/* Colored div as navbar border */}
+      <div className="h-3 bg-purple-300"></div>
+      <div className="h-3 bg-blue-200"></div>
+      <div className="mr-8">
+        <span className="text-md text-gray-500">{currentTime}</span>
+      </div>
     </>
   );
 };
