f3f15d5d726691517d9e774c858653be3f9b95e7
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 09:32:05 +0530

    shadows added

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -12,11 +12,13 @@ const BlogPost = () => {
   }
 
   return (
-    <div className="m-4 p-8 rounded-md shadow-md bg-white">
-      <h2 className="text-2xl text-center text-purple-800 title">{post.title}</h2>
+    <div className='bg-gray-100 h-screen flex justify-center items-center'>
+    <div className="m-4 p-8 rounded-lg shadow-lg bg-white block w-3/4">
+      <h2 className="text-2xl text-center font-bold text-purple-800 title">{post.title}</h2>
       <p className="text-lg">Category: {post.category}</p>
       <p className="text-lg">Context: {post.context}</p>
       {post.image && <img src={post.image} alt="" className="max-w-full h-auto mb-4" />}
+    </div>
     </div>
   );
 };
