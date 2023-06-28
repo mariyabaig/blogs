f9acd881b54c5ed525b6d09f0abf2d2fd0160c61
mariyabaig <mariyabaig0106@gmail.com>
2023-06-28 13:36:18 +0530

    navbar turned into menu for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const [currentTime, setCurrentTime] = useState("");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -16,31 +18,40 @@ const Navbar = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <nav className="shadow-lg bg-purple-200 flex justify-between font-karla">
         <div className="max-w-3xl mx-auto">
-          <div className="grid sm:grid-cols-4 text-lg gap-4 text-center py-4 px-8">
-            <div className="">
-              <Link to="/" className="hover:font-bold p-2 font-cursive text-4xl font-extrabold text-purple-950">
-                BlogAway
-              </Link>
-            </div>
-            <div>
-              <Link to="/addblogs" className="hover:font-bold p-2">
-                add blogs
-              </Link>
-            </div>
-            <div>
-              <Link to="/list" className="hover:font-bold p-2">
-                view blogs
-              </Link>
-            </div>
-            <div>
-              <Link to="/about" className="hover:font-bold p-2">
-                about
-              </Link>
-            </div>
+          <div className="flex justify-between items-center px-4 py-2">
+            <Link to="/" className="hover:font-bold p-2 font-cursive text-4xl font-extrabold text-purple-950">
+              BlogAway
+            </Link>
+            <button
+              onClick={toggleMenu}
+              className="text-purple-950 hover:text-purple-900 focus:outline-none focus:text-purple-900"
+              aria-label="Toggle menu"
+            >
+              {menuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+          <div
+            className={`${
+              menuOpen ? "flex text-sm " : "hidden"
+            } sm:flex sm:items-center sm:justify-between text-lg gap-4 text-center py-4 px-8`}
+          >
+            <Link to="/addblogs" className="hover:font-bold p-2">
+              Add Blogs
+            </Link>
+            <Link to="/list" className="hover:font-bold p-2">
+              View Blogs
+            </Link>
+            <Link to="/about" className="hover:font-bold p-2">
+              About
+            </Link>
           </div>
         </div>
         <div className="mr-8">
