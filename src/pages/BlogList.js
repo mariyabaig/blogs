ebe572bec30926c2b825021a899bb2175a0a5af6
P1033MB <mariya.baig@nagarro.com>
2024-01-07 14:17:40 +0530

    UI updated

--- src/pages/BlogList.js
+++ src/pages/BlogList.js
@@ -5,7 +5,8 @@ import {
   BsSuitHeart,
   BsSuitHeartFill,
   BsPencil,
-  BsTrash} from "react-icons/bs";
+  BsTrash,
+} from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,9 +17,9 @@ const BlogList = () => {
   const navigate = useNavigate();
   const posts = useSelector((state) => state.blogReducer.posts);
   const dispatch = useDispatch();
-  const [editMode, setEditMode] = useState(false)
+  const [editMode, setEditMode] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
-  
+
   const [editedPost, setEditedPost] = useState({
     index: null,
     title: "",
@@ -62,9 +63,9 @@ const BlogList = () => {
     });
     toast.success("Post updated successfully!", {
       className: "toast-success",
-    })
-     setShowEditModal(false)
-  }
+    });
+    setShowEditModal(false);
+  };
 
   const handleCancelEdit = () => {
     setEditMode(false);
@@ -74,8 +75,8 @@ const BlogList = () => {
       category: "",
       context: "",
       image: "",
-    })
-    setShowEditModal(false)
+    });
+    setShowEditModal(false);
   };
 
   const handleToggleLike = (index) => {
@@ -111,125 +112,121 @@ const BlogList = () => {
     return tags;
   }, []);
 
-  const filteredPosts = selectedTag === "all" ? posts : posts.filter(post => post.tags && post.tags.includes(selectedTag));
+  const filteredPosts =
+    selectedTag === "all"
+      ? posts
+      : posts.filter((post) => post.tags && post.tags.includes(selectedTag));
 
   return (
-    <>
-      <div className="bg-gray-100">
-        <h2 className="text-3xl font-bold m-2 flex justify-center font-karla ">
-          Your recent posts:
-        </h2>
-        <div className="flex justify-center flex-wrap gap-2 m-2">
-          <span
-            className="text-sm p-2 rounded-lg bg-gray-400 text-white font-bold"
-            onClick={() => handleTagClick("all")}
+    <div className="bg-gray-100 p-8">
+      <h2 className="text-3xl font-bold mb-8 text-center text-gray-800">
+        Your Recent Posts
+      </h2>
+      <div className="flex justify-center space-x-2 mb-4">
+        <button
+          className={`tag-button ${selectedTag === "all" ? "active-tag" : ""}`}
+          onClick={() => handleTagClick("all")}
+        >
+          #All
+        </button>
+        {allTags.map((tag, index) => (
+          <button
+            key={index}
+            className={`tag-button ${selectedTag === tag ? "active-tag" : ""}`}
+            onClick={() => handleTagClick(tag)}
           >
-            #All
-          </span>
-          {allTags.map((tag, index) => (
-            <span
+            #{tag}
+          </button>
+        ))}
+      </div>
+      <button
+        className="bg-purple-500 text-white font-bold py-2 px-4 rounded hover:bg-purple-600 transition-all duration-200"
+        onClick={() => navigate("/addblogs")}
+      >
+        Add New Blog
+      </button>
+      {filteredPosts && filteredPosts.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+          {filteredPosts.map((post, index) => (
+            <div
               key={index}
-              className={`text-sm p-2 rounded-lg bg-gray-400 text-white font-bold ${
-                selectedTag === tag ? "bg-gray-600" : ""
-              }`}
-              onClick={() => handleTagClick(tag)}
+              className="p-6 border rounded-lg shadow-md bg-white"
             >
-              #{tag}
-            </span>
+              <h3 className="text-2xl font-bold mb-4 text-purple-800">
+                {post.title}
+              </h3>
+              {post.category && (
+                <span className="text-md font-bold">
+                  Category: {post.category}
+                </span>
+              )}
+              <div className="mb-4">
+                <span className="tag grid grid-cols-4 gap-1">
+                  {post.tags &&
+                    post.tags.map((tag, i) => (
+                      <span
+                        key={i}
+                        className="text-md p-2 rounded-lg bg-gray-100 "
+                      >
+                        #{tag}
+                      </span>
+                    ))}
+                </span>
+              </div>
+              <MDEditor.Markdown
+                source={post.context}
+                data-color-mode="light"
+                className="mb-4"
+                style={{backgroundColor: 'white', color : 'black'}}
+              />
+              <div className="flex justify-between">
+                <div className="flex space-x-4">
+                  <button
+                    className="text-pink-600"
+                    onClick={() => handleToggleLike(index)}
+                  >
+                    {likedPosts.includes(index) ? (
+                      <BsSuitHeartFill size={25} />
+                    ) : (
+                      <BsSuitHeart size={25} />
+                    )}
+                  </button>
+                  <button
+                    className="bg-pink-500 text-white font-bold py-2 px-4 rounded hover:bg-pink-600 transition-all duration-200"
+                    onClick={() => handleEditPost(index)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-pink-500 text-white font-bold py-2 px-4 rounded hover:bg-pink-600 transition-all duration-200"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "Are you sure you want to delete this post?"
+                        )
+                      ) {
+                        handleDeletePost(index);
+                      }
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+                <button
+                  className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition-all duration-200"
+                  onClick={() => handleViewPost(index)}
+                >
+                  Show More
+                </button>
+              </div>
+            </div>
           ))}
         </div>
-        <button
-          className="bg-purple-100 p-4 text-purple-800 font-bold rounded-lg block m-3  hover:bg-purple-400 transition-all duration-200"
-          onClick={() => navigate("/addblogs")}
-        >
-          Add new blog
-        </button>
-        {filteredPosts && filteredPosts.length > 0 && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 m-4">
-            {filteredPosts.map((post, index) => (
-              <div
-                key={index}
-                className="m-4 p-8 rounded-md shadow-md bg-white card"
-              >
-                <>
-                  <div className="flex flex-col">
-                    <span className="text-2xl font-bold font-mono text-center text-purple-900 title underline">
-                      {post.title}
-                      <button
-                        className="m-2 text-red-600 shadow-sm"
-                        onClick={() => handleToggleLike(index)}
-                      >
-                        {likedPosts.includes(index) ? (
-                          <BsSuitHeartFill size={20} />
-                        ) : (
-                          <BsSuitHeart size={20} />
-                        )}
-                      </button>
-                    </span>
-                    <div className="tag">
-                      {post.tags &&
-                        post.tags.map((tag, i) => (
-                          <span
-                            key={i}
-                            className="text-sm p-2 m-2 rounded-lg bg-gray-700 text-white font-thin "
-                          >
-                            #{tag}
-                          </span>
-                        ))}
-                    </div>
-                    <div className="tag m-2">
-                      <span className="text-sm p-2 rounded-lg bg-gray-400 text-white font-bold">
-                        {post.category}
-                      </span>
-                    </div>
-                    <div className="spacer h-5" />
-                    <MDEditor.Markdown
-                      source={post.context}
-                      data-color-mode="light"
-                      style={{
-                        background: "white",
-                        color: "black",
-                        padding: "1rem",
-                        borderRadius: "0.5rem",
-                      }}
-                    />
-
-                    {/* {post.image && <img src={post.image} alt="" className="max-w-full h-auto mb-4" />} */}
-                    <span>
-                      <button
-                        className="m-2 shadow-sm  p-2 rounded-sm  edit-button"
-                        onClick={() => handleEditPost(index)}
-                      >
-                        <BsPencil size={20} />
-                      </button>
-                      <button
-                        className="m-2 shadow-sm  p-2 rounded-sm delete-button"
-                        onClick={() => {
-                          if (
-                            window.confirm(
-                              "Are you sure you want to delete this post?"
-                            )
-                          ) {
-                            handleDeletePost(index);
-                          }
-                        }}
-                      >
-                        <BsTrash size={20} />
-                      </button>
-                      <button
-                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded shadow hover:bg-blue-600 transition-all duration-200"
-                        onClick={() => handleViewPost(index)}
-                      >
-                        Show More
-                      </button>
-                    </span>
-                  </div>
-                </>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      ) : (
+        <p className="text-center text-gray-700 mt-8">
+          No posts found for the selected tag.
+        </p>
+      )}
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -243,8 +240,8 @@ const BlogList = () => {
           handleCancelEdit={handleCancelEdit}
         />
       )}
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default BlogList;
