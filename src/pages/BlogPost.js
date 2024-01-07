c1da22f7acf886da765d1b62f5c8f6119fb15a76
P1033MB <mariya.baig@nagarro.com>
2024-01-07 14:35:22 +0530

    UI updated

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -12,25 +12,9 @@ const BlogPost = () => {
   const navigate = useNavigate()
 
   if (!post) {
-    return <div>Loading...</div>; // You can replace this with an appropriate loading state
+    return <div>Loading...</div>; 
   }
 
-  const handleShare = () => {
-    setIsHovered(!isHovered);
-  };
-
-  const handleTwitterShare = () => {
-    const url = encodeURIComponent(window.location.href);
-    const twitterShareUrl = `https://twitter.com/intent/tweet?url=${url}`;
-    window.open(twitterShareUrl, "_blank");
-  };
-
-  const handleWhatsAppShare = () => {
-    const url = encodeURIComponent(window.location.href);
-    const text = encodeURIComponent(post.title);
-    const whatsAppShareUrl = `https://api.whatsapp.com/send?text=${text}%20${url}`;
-    window.open(whatsAppShareUrl, "_blank");
-  };
 
   return (
     <>
@@ -38,16 +22,16 @@ const BlogPost = () => {
         <div className="m-4 p-8 rounded-lg shadow-lg bg-white block w-3/4 ">
           <button
             onClick={() => navigate("/list")}
-            className="bg-purple-100 p-2 rounded text-purple-700  hover:bg-purple-400 transition-all duration-200"
+            className="bg-pink-500 text-white font-bold py-2 px-4 rounded hover:bg-pink-600 transition-all duration-200"
           >
             Back
           </button>
-          <h2 className="text-2xl text-center font-bold text-purple-800 title">
+          <h2 className="text-2xl text-center font-bold text-pink-500 title">
             {post.title}
           </h2>
           <div className="tag">
-            <span className="text-sm p-2 rounded-lg bg-gray-400 text-white font-bold">
-              {post.category}
+            <span className="text-md p-2 rounded-lg  font-bold">
+              Category: {post.category}
             </span>
           </div>
           <div className="spacer h-5" />
@@ -65,31 +49,6 @@ const BlogPost = () => {
           {post.image && (
             <img src={post.image} alt="" className="max-w-full h-auto mb-4" />
           )}
-          <div className="flex justify-center">
-            <div
-              className="m-2 cursor-pointer flex p-2"
-              onClick={handleShare}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              {isHovered ? (
-                <>
-                  <FaTwitter
-                    size={32}
-                    color="#1DA1F2"
-                    onClick={handleTwitterShare}
-                  />
-                  <FaWhatsapp
-                    size={32}
-                    color="#25D366"
-                    onClick={handleWhatsAppShare}
-                  />
-                </>
-              ) : (
-                <FaShareAlt size={22} />
-              )}
-            </div>
-          </div>
         </div>
       </div>
     </>
