39fb3c2cf16139df77efc848301d4e93e80e1a5f
P1033MB <mariya.baig@nagarro.com>
2024-01-07 12:47:24 +0530

    routes refactored

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link, Routes, BrowserRouter, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Blogs from './pages/Blogs';
-import About from './pages/About';
-import List from './pages/List';
+import AddBlogs from './pages/AddBlogs';
+import AboutUs from './pages/AboutUs';
+import BlogList from './pages/BlogList';
 import Navbar from './components/Navbar';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,10 +16,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/addblogs" element={<Blogs />} />
-          <Route exact path="/about" element={<About />} />
-          <Route eaxct path="/list" element={<List />} />
-          <Route path="/blogs/:index" element={<BlogPost/>} />
+          <Route exact path="/addblogs" element={<AddBlogs />} />
+          <Route exact path="/about" element={<AboutUs />} />
+          <Route eaxct path="/list" element={<BlogList />} />
+          <Route path="/blogs/:index" element={<BlogPost />} />
         </Routes>
         <ToastContainer />
       </BrowserRouter>
