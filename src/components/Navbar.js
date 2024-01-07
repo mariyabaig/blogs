fc5595781d4b144f26965fd8b8de4e4a9c4029d5
P1033MB <mariya.baig@nagarro.com>
2024-01-07 14:39:02 +0530

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,53 +24,57 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-purple-200">
-        <div className="max-w-3xl mx-auto px-4 py-2 flex justify-between items-center">
+      <nav className="bg-gray-100  py-4">
+        <div className="container mx-auto flex justify-between items-center">
           <Link
             to="/"
-            className="text-4xl font-bold text-purple-950 hover:font-bold font-cursive"
+            className="text-4xl font-bold text-blue-500 hover:font-bold font-cursive"
           >
             BlogAway
           </Link>
           <button
             onClick={toggleMenu}
-            className="text-purple-950 hover:text-purple-900 focus:outline-none focus:text-purple-900 lg:hidden"
+            className="text-blue-500 hover:text-blue-500 focus:outline-none lg:hidden"
             aria-label="Toggle menu"
           >
             {menuOpen ? <FaTimes /> : <FaBars />}
           </button>
-          <div className="hidden lg:flex lg:items-center lg:justify-between text-lg gap-4 text-center py-4 px-8 md:flex">
-          
-            <Link to="/addblogs" className="hover:font-bold" >
+          <div className="hidden lg:flex lg:items-center lg:justify-between text-lg gap-4 text-center py-4 px-8">
+            <Link to="/addblogs" className="text-blue-500 hover:font-bold">
               Add Blogs
             </Link>
-            <Link to="/list" className="hover:font-bold">
+            <Link to="/list" className="text-blue-500 hover:font-bold">
               View Blogs
             </Link>
-            <Link to="/about" className="hover:font-bold">
+            <Link to="/about" className="text-blue-500 hover:font-bold">
               About
             </Link>
-            <h1 className="text-sm ml-16">{currentTime}</h1> 
+            <span className="text-md text-blue-500 ml-6">{currentTime}</span>
           </div>
         </div>
         {menuOpen && (
-          <div className="lg:hidden bg-gray-100 py-4 px-8">
-            <Link to="/addblogs" className="block py-2">
+          <div className="lg:hidden bg-gray-100 py-4">
+            <Link
+              to="/addblogs"
+              className="block py-2 text-purple-500 hover:font-bold"
+            >
               Add Blogs
             </Link>
-            <Link to="/list" className="block py-2">
+            <Link
+              to="/list"
+              className="block py-2 text-purple-500 hover:font-bold"
+            >
               View Blogs
             </Link>
-            <Link to="/about" className="block py-2">
+            <Link
+              to="/about"
+              className="block py-2 text-purple-500 hover:font-bold"
+            >
               About
             </Link>
-           
           </div>
         )}
       </nav>
-      <div className="h-3 bg-purple-300"></div>
-      <div className="h-3 bg-blue-200"></div>
-     
     </>
   );
 };
